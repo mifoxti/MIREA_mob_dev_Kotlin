eed1e547d3d90e1fce778e82396d52f66750d105
mifoxti <52703409+mifoxti@users.noreply.github.com>
2024-11-12 22:36:50 +0300

    six prac

---
+++ 6prac/app/src/main/java/com/example/a6prac/ui/theme/Theme.kt
@@ -0,0 +1,48 @@
+package com.example.a6prac.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = LightGreen,
+    secondary = ButtonGreen,
+    onSecondary = TextWhite,
+    tertiary = DarkGreen
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = LightGreen,
+    secondary = ButtonGreen,
+    onSecondary = TextWhite,
+    tertiary = DarkGreen
+)
+
+@Composable
+fun _6pracTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
